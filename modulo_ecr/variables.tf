7fba17929078e7096f0f0309128e664a3c09dbcd
Jorge382 <jorge.vidal@campusdual.com>
2025-01-31 11:42:17 +0100

    Añadiendo gitignore

--- modulo_ecr/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "repository_name" {
-  description = "The name of the ECR repository"
-  type        = string
-}
-
-variable "policy_document" {
-  description = "The JSON policy document for the ECR repository"
-  type        = string
-}
